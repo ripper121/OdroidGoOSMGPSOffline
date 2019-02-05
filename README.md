5f965e42f9c268c82d0a3f1300c532ce98047500
ripper121 <sts@zwave.eu>
2019-02-05 10:34:46 +0100

    TileDownloader

--- README.md
+++ README.md
@@ -1,34 +1,33 @@
 # OdroidGoOSMGPSOffline
 Odroid Go OSM GPS Offline
 
-   GPS Module Connection (Neo-6M Module)
-   
-   GND|TX|RX|VCC
-   
-   GND|04|05|P3V3
-   
-   ODROID-GO Header(P2)
+GPS Module Connection (Neo-6M Module)
+GND|TX|RX|VCC
+GND|04|05|P3V3
+ODROID-GO Header(P2)
    
    
 How to generate Tiles (offline Map) for your Area:
-1. Open Maperitive.exe (http://maperitive.net/)
-2. Move the map to your favor position
-3. MAP->Set Geometry Bounds
-4. MAP->Set Printing Bounds
-Now the area of the map you want to Export is set.
-5. TOOLS-> Generate Tiles (this can take some time, depending on how high your Zoom Level is)
-6. Now you can find some png files in the Maperitive /tiles folder
-8. Open Flexxi.exe (https://sourceforge.net/projects/flexxi-image-resizer/)
-9. Import the Tiles Folder
-10. Resize the Image to 240x240 px (best Fit for GO Screen)
-11. Convert to JPG Files
-12. Start
-13. Now you have your tiles in the correct size and format
-14. Copy the "TILES" (all Uppercase) folder to the root directory of your SD card
+C:\>TileDownloader.exe
+URL:
+-URL TileServer
+Zoom Leve:
+-z ZOOM
+
+Bounding Box:
+left bottom
+-left >min-longitude
+-bottom >min-latitude
+right top
+-right >max-longitude
+-top >max-latitude
+
+Example >>TileDownloader.exe -URL https://a.tile.openstreetmap.org/${z}/${x}/${y}.png -z 5 -left -0.489 -botton 51.28 -right 0.236 -top 51.686
+Tile servers: https://wiki.openstreetmap.org/wiki/Tile_servers
+
+Example for Zoomlevel 5 of London >>TileDownloader.exe -z 5 -left -0.489 -botton 51.28 -right 0.236 -top 51.686
+If your arguments look like that "Arguments: -z 5 -left -489 -botton 0 -right 236 -top 51686" use a ',' instead of a '.' (depends on your region setting.)
 
 
 Forum Thread: https://forum.odroid.com/viewtopic.php?f=162&t=33629
-
-Download Tools: https://drive.google.com/open?id=18JUOutDZzVzfiJgyUtiNcj4cO6Xv8Wn8
-
 Download TILES (sample set): https://drive.google.com/open?id=1ng5wpFVR5ual7zt3KRj-fZQsR8js7pFB
---
+++ TileDownloader/src/TileDownloader/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+namespace TileDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            double zoom = 0;
+
+            double left = 0, bottom = 0, right = 0, top = 0;
+            String sourceURL = "";
+            
+            if (args == null)
+            {
+
+            }
+            else
+            {
+                if (args.Length <= 0)
+                {
+                    Console.WriteLine("URL:\n-URL TileServer\nZoom Leve:\n-z ZOOM\n\nBounding Box:\nleft bottom\n-left >min-longitude\n-bottom >min-latitude\nright top\n-right >max-longitude\n-top >max-latitude\n");
+                    Console.WriteLine("Example >>TileDownloader.exe -URL https://a.tile.openstreetmap.org/${z}/${x}/${y}.png -z 5 -left -0.489 -botton 51.28 -right 0.236 -top 51.686");
+                    Console.WriteLine("Tile servers: https://wiki.openstreetmap.org/wiki/Tile_servers");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    byte argCount = 0;
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        string argument = args[i];
+                        if (argument.Equals("-z"))
+                        {
+                            zoom = Convert.ToDouble(args[i + 1]);
+                            argCount++;
+                        }
+
+                        if (argument.Equals("-left"))
+                        {
+                            left = Convert.ToDouble(args[i + 1]);
+                            argCount++;
+                        }
+
+                        if (argument.Equals("-bottom"))
+                        {
+                            bottom = Convert.ToDouble(args[i + 1]);
+                            argCount++;
+                        }
+
+                        if (argument.Equals("-right"))
+                        {
+                            right = Convert.ToDouble(args[i + 1]);
+                            argCount++;
+                        }
+
+                        if (argument.Equals("-top"))
+                        {
+                            top = Convert.ToDouble(args[i + 1]);
+                            argCount++;
+                        }
+
+                        if (argument.Equals("-URL"))
+                        {
+                            sourceURL = args[i + 1];
+                            argCount++;
+                        }
+                    }
+
+                    if (argCount < 5)
+                    {
+                        Console.WriteLine("To few Arguments");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Arguments: -URL " + sourceURL + " -z " + zoom + " -left " + left + " -botton " + bottom + " -right " + right + " -top " + top);
+                    }
+                }
+            }
+               
+            
+            Int32 tileLeft = Convert.ToInt32(long2tilex(left, zoom));
+            Int32 tileBottom = Convert.ToInt32(lat2tiley(bottom, zoom));
+            Int32 tileRight = Convert.ToInt32(long2tilex(right, zoom));
+            Int32 tileTop = Convert.ToInt32(lat2tiley(top, zoom));
+            double tileCount = 0, downloadCount = 0;
+
+            for (int tileX = tileLeft; tileX <= tileRight; tileX++)
+            {
+                for (int tileY = tileTop; tileY <= tileBottom; tileY++)
+                {
+                    tileCount++;
+                }
+            }
+            Console.WriteLine("Download Tile Count:" + tileCount + "? Y/N");
+
+            ConsoleKeyInfo info = Console.ReadKey();
+            if (info.KeyChar == 'y' || info.KeyChar == 'Y')
+            {
+                for (int tileX = tileLeft; tileX <= tileRight; tileX++)
+                {
+                    for (int tileY = tileTop; tileY <= tileBottom; tileY++)
+                    {
+                        String URL = sourceURL.Replace("${z}", zoom.ToString());
+                        URL = URL.Replace("${x}", tileX.ToString());
+                        URL = URL.Replace("${y}", tileY.ToString());
+
+                        if (!Download(URL, zoom, tileX, tileY))
+                        {
+                            Console.WriteLine("Download Error:" + URL);
+                        }
+                        else
+                        {
+                            downloadCount++;
+                            Console.WriteLine("Download:" + downloadCount + "/" + tileCount);
+                        }
+                    }
+                }
+
+                Console.WriteLine("Download Finished");
+            }
+            Environment.Exit(0);
+
+        }
+
+        static bool Download(String URL, double zoom, double tileX, double tileY)
+        {
+            try
+            {
+                String filePath = AppDomain.CurrentDomain.BaseDirectory + "TILES/" + zoom + "/" + tileX + "/" + tileY + ".jpg";
+
+                if (!File.Exists(filePath))
+                {
+                    if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "/TILES"))
+                    {
+                        Directory.CreateDirectory("/TILES");
+                    }
+
+                    if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "/TILES/" + zoom))
+                    {
+                        Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "/TILES/" + zoom);
+                    }
+
+                    if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "/TILES/" + zoom + "/" + tileX))
+                    {
+                        Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "/TILES/" + zoom + "/" + tileX);
+                    }
+
+                    if (!Directory.Exists(filePath))
+                        SaveImage(URL, filePath, ImageFormat.Jpeg, 240, 240, false);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error:" + e.Message);
+                return false;
+            }
+        }
+
+        static double long2tilex(double lon, double z)
+        {
+            return (double)((lon + 180.0) / 360.0 * Math.Pow(2.0, z));
+        }
+
+        static double lat2tiley(double lat, double z)
+        {
+            return (double)((1.0 - Math.Log(Math.Tan(lat * Math.PI / 180.0) + 1.0 / Math.Cos(lat * Math.PI / 180.0)) / Math.PI) / 2.0 * Math.Pow(2.0, z));
+        }
+
+        static Image ResizeImage(Image image, int width, int height, bool preserveAspectRatio = true)
+        {
+            int newWidth;
+            int newHeight;
+            if (preserveAspectRatio)
+            {
+                int originalWidth = image.Width;
+                int originalHeight = image.Height;
+                float percentWidth = (float)width / (float)originalWidth;
+                float percentHeight = (float)height / (float)originalHeight;
+                float percent = percentHeight < percentWidth ? percentHeight : percentWidth;
+                newWidth = (int)(originalWidth * percent);
+                newHeight = (int)(originalHeight * percent);
+            }
+            else
+            {
+                newWidth = width;
+                newHeight = height;
+            }
+            Image newImage = new Bitmap(newWidth, newHeight);
+            using (Graphics graphicsHandle = Graphics.FromImage(newImage))
+            {
+                graphicsHandle.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphicsHandle.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+            return newImage;
+        }
+
+        static void SaveImage(String imageUrl, string filename, ImageFormat format, int width, int height, bool preserveAspectRatio = true)
+        {
+
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead(imageUrl);
+            Bitmap bitmap; bitmap = new Bitmap(stream);
+
+
+            Image outputImage = ResizeImage(bitmap, width, height, false);
+
+            if (outputImage != null)
+                outputImage.Save(filename, format);
+
+            stream.Flush();
+            stream.Close();
+            client.Dispose();
+        }
+    }
+}
